e88c924eaf91acf7c857ca25b9306f8e59cca664
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2023-04-18 01:46:50 +0200

    azure

--- src/main/java/com/cokroktosmok/beersandmealsappfront/BeersAndMealsAppFrontApplication.java
+++ src/main/java/com/cokroktosmok/beersandmealsappfront/BeersAndMealsAppFrontApplication.java
@@ -3,11 +3,16 @@ package com.cokroktosmok.beersandmealsappfront;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class BeersAndMealsAppFrontApplication {
+public class BeersAndMealsAppFrontApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BeersAndMealsAppFrontApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BeersAndMealsAppFrontApplication.class);
     }
 }
